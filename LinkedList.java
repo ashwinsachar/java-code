e1623665e93b9cae787bff7c49b6da5d78eff732
Ashwin Achar <ashwinachar@Ashwins-MacBook-Pro.local>
2015-12-14 03:33:42 -0600

    LinkedList Operations

--- LinkedList.java
+++ LinkedList.java
@@ -69,25 +69,59 @@ public class LinkedList {
 //		n.next = newNode; 
 		return;
 	}
+	// Delete node given a key
+	public void deleteNode(int key){
+		//Check if the head has the key to be deleted
+		if (head.data==key){
+			head.next=head;
+			}
+		Node n = head;
+		while (n.next != null && n.next.data !=key){
+			n=n.next;
+		}
+		n.next=n.next.next;
+	}
+	
+	// Delete a node given a position
+	public void deleteNodeByPosition(int pos){
+		// Check if head is to be deleted, given by position = 0
+		if (pos == 0 && head != null && head.next != null){
+			head = head.next;
+			return;
+		}
+		Node n = head;
+		for (int i = 1; i<pos; i++){
+			n = n.next;
+		}
+		n.next = n.next.next;
+		return;
+	}
 	
 	public static void main(String[] args) {
 		LinkedList ll = new LinkedList();
 		ll.head = new Node(0);
 		Node n1 = new Node(1);
 		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		Node n4 = new Node(4);
+		Node n5 = new Node(5);
 		
 		ll.head.next = n1;
 		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
 		
-		Node givenNode = n1;
+//		Node givenNode = n1;
 		
 //		Node newNode = new Node(-1);
 //		ll.pushBeforeHead(newNode);
 //		ll.pushAfterLastNode(newNode);
-		Node newNode1 = new Node(3);
+//		Node newNode1 = new Node(3);
 //		ll.pushAfterAnyNode(givenNode, newNode1);
-		ll.pushBeforeAnyNode(givenNode, newNode1);
-		
+//		ll.pushBeforeAnyNode(givenNode, newNode1);
+//		ll.deleteNode(1);
+		ll.deleteNodeByPosition(3);
 		ll.display();
 		
 
