687884db6397519f558ed2f199232b09d9ce9581
Ashwin Achar <ashwinachar@Ashwins-MacBook-Pro.local>
2015-12-16 23:11:39 -0600

    DoublyLinkedList Operations

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,57 +1,63 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class DoublyLinkedList {
 	Node head;
-	static class Node{
+
+	static class Node {
 		int data;
 		Node next;
 		Node prev;
-		Node(int d){
+
+		Node(int d) {
 			data = d;
 			next = null;
 			prev = null;
 		}
 	}
-	
-	public void display(){
+
+	public void display() {
 		Node n = head;
-		while (n != null){
-			System.out.println("Data at the current node is "+n.data);
+		while (n != null) {
+			System.out.println("Data at the current node is " + n.data);
 			n = n.next;
 		}
 	}
-	
-	public void insertBeforeHead(Node newnode){
+
+	public void insertBeforeHead(Node newnode) {
 		head.prev = newnode;
 		newnode.next = head;
 		head = newnode;
 		newnode.prev = null;
 	}
-	
-	public void insertAfterAnyNode(Node newnode, Node givennode){
-		if(head == null){
+
+	public void insertAfterAnyNode(Node newnode, Node givennode) {
+		if (head == null) {
 			head = newnode;
 			return;
 		}
 	}
-	
-	public void deleteNode(Node givennode){
-		if(head == null){
+
+	public void deleteNode(Node givennode) {
+		if (head == null) {
 			return;
 		}
-		if (head == givennode){
+		if (head == givennode) {
 			head.next.prev = null;
 			head.next = head;
 		}
 		Node curr = head;
-		while (curr != givennode && curr != null){
+		while (curr != givennode && curr != null) {
 			curr = curr.next;
 		}
-		if(curr.next != null){
+		if (curr.next != null) {
 			curr.next.prev = curr.prev;
 		}
 		curr.prev.next = curr.next;
 		return;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DoublyLinkedList d1 = new DoublyLinkedList();
@@ -61,35 +67,68 @@ public class DoublyLinkedList {
 		Node n4 = new Node(40);
 		Node n5 = new Node(50);
 		Node n6 = new Node(60);
-		
+
 		d1.head.next = n2;
-//		d1.head.prev = n6;
+		// d1.head.prev = n6;
 		d1.head.prev = null;
-		
+
 		n2.next = n3;
 		n2.prev = d1.head;
-		
+
 		n3.next = n4;
 		n3.prev = n2;
-		
+
 		n4.next = n5;
 		n4.prev = n3;
-		
+
 		n5.next = n6;
 		n5.prev = n4;
-		
+
 		n6.next = null;
-//		n6.next = head;
+		// n6.next = head;
 		n6.prev = n5;
-		
+
 		Node newNode = new Node(70);
 		int k = 50;
-		
-//		System.out.println("\nBefore Deletion\n");
-//		d1.display();
-//		d1.deleteNode(n3);
-//		System.out.println("\nAfter Deletion\n");
-		d1.display();
+
+		// System.out.println("\nBefore Deletion\n");
+		// d1.display();
+		// d1.deleteNode(n3);
+		// System.out.println("\nAfter Deletion\n");
+		// d1.display();
+
+		List<Integer> arrayList = new ArrayList<Integer>();
+		List<Integer> linkedList = new LinkedList<Integer>();
+
+		doTimings("ArrayList", arrayList);
+		doTimings("LinkedList", linkedList);
+
+	}
+
+	public static void doTimings(String type, List<Integer> list) {
+
+		for (int i = 0; i <= 1E5; i++) {
+			list.add(i);
+		}
+
+		long start = System.currentTimeMillis();
+
+		// Remove items from the list
+		// list.remove(1E5);
+
+		// Add items at the end of list
+		/*
+		 * for (int i = 0; i <= 1E5; i++) { list.add(i); }
+		 */
+
+		// Add items to the beginning of the list
+		/*
+		 * for (int i = 0; i <= 1E5; i++) { list.add(0, i); }
+		 */
+
+		long end = System.currentTimeMillis();
+
+		System.out.println("Time taken: " + (end - start) + "ms by " + type);
 	}
 
 }
